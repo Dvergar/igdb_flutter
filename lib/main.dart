7337fa17993fa66ad654188388e663b2d02cb1c4
Dvergar <laurent.rd@gmail.com>
2020-01-29 13:55:48 +0100

    app title changed

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IGDB Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'IGDB Flutter'),
     );
   }
 }
