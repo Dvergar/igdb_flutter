cb8b476610196ec46a5cac1ad3d50f6fe0358ea6
Dvergar <laurent.rd@gmail.com>
2020-02-03 17:51:27 +0100

    padding on game screen

--- lib/game.dart
+++ lib/game.dart
@@ -44,60 +44,63 @@ class _GameState extends State<Game> {
                   ),
                 ),
                 child: SafeArea(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: <Widget>[
-                      Hero(
-                          tag: "game-name-${widget.index}",
-                          child: Material(
-                              type: MaterialType.transparency,
-                              child: Text(widget.entry.name,
-                                  style: TextStyle(fontSize: 30)))),
-                      StreamBuilder(
-                        stream: gameBloc.stream,
-                        builder:
-                            (BuildContext context, AsyncSnapshot snapshot) {
-                          if (!snapshot.hasData) return Container();
+                  child: Padding(
+                    padding: const EdgeInsets.all(9.0),
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: <Widget>[
+                        Hero(
+                            tag: "game-name-${widget.index}",
+                            child: Material(
+                                type: MaterialType.transparency,
+                                child: Text(widget.entry.name,
+                                    style: TextStyle(fontSize: 30)))),
+                        StreamBuilder(
+                          stream: gameBloc.stream,
+                          builder:
+                              (BuildContext context, AsyncSnapshot snapshot) {
+                            if (!snapshot.hasData) return Container();
 
-                          var gameEntry = snapshot.data as GameEntry;
-                          var ratingColor = Colors.red;
-                          if (gameEntry.rating > 39)
-                            ratingColor = Colors.orange;
-                          if (gameEntry.rating > 59) ratingColor = Colors.green;
+                            var gameEntry = snapshot.data as GameEntry;
+                            var ratingColor = Colors.red;
+                            if (gameEntry.rating > 39)
+                              ratingColor = Colors.orange;
+                            if (gameEntry.rating > 59) ratingColor = Colors.green;
 
-                          return Column(
-                            children: <Widget>[
-                              SizedBox(height:20),
-                              CircularPercentIndicator(
-                                radius: 120.0,
-                                lineWidth: 13.0,
-                                animation: true,
-                                percent: gameEntry.rating / 100,
-                                center: new Text(
-                                  gameEntry.rating.round().toString(),
-                                  style: new TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 20.0),
+                            return Column(
+                              children: <Widget>[
+                                SizedBox(height:20),
+                                CircularPercentIndicator(
+                                  radius: 120.0,
+                                  lineWidth: 13.0,
+                                  animation: true,
+                                  percent: gameEntry.rating / 100,
+                                  center: new Text(
+                                    gameEntry.rating.round().toString(),
+                                    style: new TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20.0),
+                                  ),
+                                  footer: new Text(
+                                    "Ratings",
+                                    style: new TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 17.0),
+                                  ),
+                                  circularStrokeCap: CircularStrokeCap.round,
+                                  progressColor: ratingColor,
                                 ),
-                                footer: new Text(
-                                  "Ratings",
-                                  style: new TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 17.0),
-                                ),
-                                circularStrokeCap: CircularStrokeCap.round,
-                                progressColor: ratingColor,
-                              ),
-                              SizedBox(height:20),
-                              Container(
-                                padding: EdgeInsets.all(15),
-                                color: Colors.black.withOpacity(0.8),
-                                child: Text(gameEntry.summary, style: TextStyle(fontSize:20),))
-                            ],
-                          );
-                        },
-                      ),
-                    ],
+                                SizedBox(height:20),
+                                Container(
+                                  padding: EdgeInsets.all(15),
+                                  color: Colors.black.withOpacity(0.4),
+                                  child: Text(gameEntry.summary, style: TextStyle(fontSize:20),))
+                              ],
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 )),
           ),
