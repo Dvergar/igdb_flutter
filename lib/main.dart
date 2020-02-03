3ea949fcfc7a01b2c4ba72cbae199a67c5331cce
Dvergar <laurent.rd@gmail.com>
2020-02-03 10:16:24 +0100

    roundy layout

--- lib/main.dart
+++ lib/main.dart
@@ -54,13 +54,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                color: Color(0xff424242),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+                padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18.0),
+                    color: Color(0xff424242)),
                 child: TextField(
                   onSubmitted: (entry) => searchBloc.search(entry),
                   decoration: InputDecoration(
@@ -69,91 +71,100 @@ class _MyHomePageState extends State<MyHomePage> {
                       hintText: "SEARCH GAMES",
                       hintStyle: TextStyle(fontSize: 20)),
                 )),
-          ),
-          Expanded(
-            child: StreamBuilder(
-                stream: searchBloc.stream,
-                builder: (context, snapshot) {
-                  var searchEntry = snapshot.data as List<SearchEntry>;
-                  return snapshot.data == null
-                      ? Center(child: CircularProgressIndicator())
-                      : Container(
-                          child: ListView.builder(
-                              itemCount: searchEntry.length,
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 2.0),
-                              itemBuilder: (context, index) {
-                                var releasedYear =
-                                    searchEntry[index].releaseDate;
+                SizedBox(height:20),
+            Expanded(
+              child: StreamBuilder(
+                  stream: searchBloc.stream,
+                  builder: (context, snapshot) {
+                    var searchEntry = snapshot.data as List<SearchEntry>;
+                    return snapshot.data == null
+                        ? Center(child: CircularProgressIndicator())
+                        : Container(
+                            child: ListView.builder(
+                                itemCount: searchEntry.length,
+                                itemBuilder: (context, index) {
+                                  var releasedYear =
+                                      searchEntry[index].releaseDate;
 
-                                return Card(
-                                  child: Container(
-                                    padding: EdgeInsets.all(8),
-                                    decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                        colorFilter: ColorFilter.mode(
-                                            Colors.grey, BlendMode.darken),
-                                        image: NetworkImage(
-                                            searchEntry[index].screenshot ?? searchEntry[index].cover ?? 'https://via.placeholder.com/150/FFFF00/000000'),
-                                        fit: BoxFit.fitWidth,
-                                        alignment: Alignment.topCenter,
+                                  return Card(
+                                    child: Container(
+                                      height: 120,
+                                      padding: EdgeInsets.all(9),
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(18.0),
+                                        image: DecorationImage(
+                                          colorFilter: ColorFilter.mode(
+                                              Colors.grey, BlendMode.darken),
+                                          image: NetworkImage(searchEntry[index]
+                                                  .screenshot ??
+                                              searchEntry[index].cover ??
+                                              'https://via.placeholder.com/150/FFFF00/000000'),
+                                          fit: BoxFit.cover,
+                                          alignment: Alignment.topCenter,
+                                        ),
+                                      ),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            searchEntry[index].name,
+                                            style: TextStyle(
+                                                fontSize: 25,
+                                                fontWeight: FontWeight.w600),
+                                            maxLines: 1,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceBetween,
+                                            children: <Widget>[
+                                              InputChip(
+                                                backgroundColor: Colors.amber,
+                                                // avatar: Icon(Icons.av_timer),
+                                                label: Text(
+                                                  searchEntry[index]
+                                                      .platform
+                                                      .name,
+                                                  style: TextStyle(
+                                                      fontSize: 15,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                ),
+                                                onPressed: () {},
+                                              ),
+                                              InputChip(
+                                                backgroundColor: Colors.black,
+                                                // avatar: Icon(Icons.av_timer),
+                                                label: Text(
+                                                  releasedYear,
+                                                  style:
+                                                      TextStyle(fontSize: 15),
+                                                ),
+                                                onPressed: () {},
+                                              ),
+                                            ],
+                                          ),
+                                        ],
                                       ),
                                     ),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                          searchEntry[index].name,
-                                          style: TextStyle(fontSize: 25),
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceBetween,
-                                          children: <Widget>[
-                                            InputChip(
-                                              backgroundColor: Colors.amber,
-                                              // avatar: Icon(Icons.av_timer),
-                                              label: Text(
-                                                searchEntry[index]
-                                                    .platform
-                                                    .name,
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
-                                              onPressed: () {},
-                                            ),
-                                            InputChip(
-                                              backgroundColor: Colors.black,
-                                              // avatar: Icon(Icons.av_timer),
-                                              label: Text(
-                                                releasedYear,
-                                                style: TextStyle(fontSize: 15),
-                                              ),
-                                              onPressed: () {},
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
 
-                                  // Stack(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                  elevation: 5,
-                                  margin: EdgeInsets.all(10),
-                                );
-                              }),
-                        );
-                }),
-          ),
-        ],
+                                    // Stack(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(18.0),
+                                    ),
+                                    elevation: 5,
+                                    margin: EdgeInsets.symmetric(vertical: 10),
+                                  );
+                                }),
+                          );
+                  }),
+            ),
+          ],
+        ),
       ),
     );
   }
