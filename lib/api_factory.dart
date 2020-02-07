22839e4ad685e5e92bdf84e7d10787833350472d
Dvergar <laurent.rd@gmail.com>
2020-02-07 17:08:03 +0100

    cleanup

--- lib/api_factory.dart
+++ lib/api_factory.dart
@@ -2,18 +2,13 @@ import 'package:firebase_remote_config/firebase_remote_config.dart';
 
 String apiKey;
 
-class ApiFactory
-{
-  static Future<String> loadKey() async
-  {
+class ApiFactory {
+  static Future<String> loadKey() async {
     final RemoteConfig remoteConfig = await RemoteConfig.instance;
     await remoteConfig.fetch();
     await remoteConfig.activateFetched();
 
-    // await Future.delayed(const Duration(seconds : 5));
-
     var apiKey = remoteConfig.getString('igdb_api_key');
-    print("APIKEy $apiKey");
 
     return apiKey;
   }
